732c8a5de01a0c76878b3eb921a0dfbd957bdfd4
Manan <gouhari21@hotmail.com>
2019-10-20 19:39:50 +0530

    Working on dashboard

--- views/dashboard.py
+++ views/dashboard.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 dashboard = Blueprint('dashboard', __name__)
 
 
-@login_required
+
 @dashboard.route('/home')
+@login_required
 def home():
+    print(current_user)
     return render_template('user/dash.html')
