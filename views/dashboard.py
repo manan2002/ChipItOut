8902ed01b156769df84e1adfe8d4e289f6abfbb7
Manan <gouhari21@hotmail.com>
2019-10-24 23:57:43 +0530

    Some weird templating issue in dash.html

--- views/dashboard.py
+++ views/dashboard.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from models.address import AddressModel
+from exts import db
 
 dashboard = Blueprint('dashboard', __name__)
 
@@ -10,8 +11,9 @@ dashboard = Blueprint('dashboard', __name__)
 @login_required
 def dash():
     context = {
-        'user' : current_user
+        'u' : current_user
     }
+    print(len(current_user.addresses))
     return render_template('user/dash.html', **context)
 
 @dashboard.route('/pickup', methods = ['POST', 'GET'])
@@ -40,12 +42,44 @@ def add_address():
   
     if request.method == 'POST':
         addr = request.form.get('addr')
-        zone = addr = request.form.get('zone')
+        zone = request.form.get('zone')
+        default = request.form.get('default')
         u = current_user
-        print(addr, zone)
-        #address = AddressModel(_address=addr, zone=zone, user=current_user)
-        #address.save()
-        #
-        # return redirect(url_for('dashboard.dash'))
+        if default == 'on':
+            for address in u.addresses:
+                address.default = False
+            db.session.commit()
+            address = AddressModel(
+                _address=addr, 
+                zone=zone, 
+                default = True,
+                user=current_user)
+        else:
+            if len(u.addresses) == 0:
+                address = AddressModel(
+                    _address=addr,
+                    zone=zone, 
+                    user=current_user, 
+                    default = True)
+            else:
+                address = AddressModel(
+                    _address=addr,
+                    zone=zone,
+                    user=current_user)
+
+        address.save()
+        return redirect(url_for('dashboard.dash'))
 
     return render_template('user/add_address.html')
+
+
+@dashboard.route('/delete_address/<int:addr_id>', methods=['GET'])
+@login_required
+def del_address(addr_id):
+    addr = AddressModel.query.get(addr_id)
+    if current_user == addr.user:
+        db.session.delete(addr)
+        db.session.commit()
+        return redirect(url_for('dashboard.settings'))
+    return redirect(url_for('index'))
+
